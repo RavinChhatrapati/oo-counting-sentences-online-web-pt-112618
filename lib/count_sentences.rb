d8b23c67d9b7ac73057cc80e55949044948a177c
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-03 18:13:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,13 +1,14 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    "Hi, my name is Sophie.".end_with?(".")
+
   end
 
   def question?
-
+"What's your name?".end_with?("?")
   end
 
   def exclamation?
